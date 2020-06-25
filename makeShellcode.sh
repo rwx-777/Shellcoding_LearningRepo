3c4b6de03e5bb0cfff6bfb14527eb5a3ad0f5602
rwx777 <joshuajared007@gmail.com>
2020-06-25 11:36:03 +0200

    Add files via upload

---
+++ makeShellcode.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+objdump -M intel -D hello-world | grep '[0-9a-f]:' | grep -v 'file' | cut -f2 -d: | cut -f1-7 -d' ' | tr -s ' ' | tr '\t' ' ' | sed 's/ $//g' | sed 's/ /\\\x/g' | paste -d '' -s
+
