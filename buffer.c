719ae198cae54948768805134d48156d47e59870
rwx777 <joshuajared007@gmail.com>
2020-07-03 12:20:35 +0200

    Ch7-228

---
+++ buffer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int copytobuffer(char* input) {
+	char buffer[256];
+	strcpy (buffer, input);
+	return 0;
+}
+
+void main( int argc, char* argv[]) {
+	int local_varibale = 1;
+	copytobuffer(argv[1]);
+	exit(0);
+}
+
--- makeShellcode.sh
+++ makeShellcode.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-objdump -M intel -D hello-world | grep '[0-9a-f]:' | grep -v 'file' | cut -f2 -d: | cut -f1-7 -d' ' | tr -s ' ' | tr '\t' ' ' | sed 's/ $//g' | sed 's/ /\\\x/g' | paste -d '' -s
+file=$1
+objdump -M intel -D $1 | grep '[0-9a-f]:' | grep -v 'file' | cut -f2 -d: | cut -f1-7 -d' ' | tr -s ' ' | tr '\t' ' ' | sed 's/ $//g' | sed 's/ /\\\x/g' | paste -d '' -s
 
